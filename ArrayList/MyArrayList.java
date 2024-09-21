64f4afc5353dc4ab142241e6e78c89379ba77f25
Abdullah-CUI <abdullahdev88@gmail.com>
2024-09-21 11:55:29 +0500

    list element remove Method

--- ArrayList/MyArrayList.java
+++ ArrayList/MyArrayList.java
@@ -27,6 +27,7 @@ public class MyArrayList {
 
 		System.out.println("get all list Element");
 		for(int i=0; i<list.size(); i++) {
+
 			System.out.print(list.get(i)+ " ");
 		}
 
@@ -37,19 +38,41 @@ public class MyArrayList {
 		System.out.print("Enter Number of Element:");
 		int arrayNumber=reader.nextInt();
 
-		for(int i=0; i<=arrayNumber; i++){
-			System.out.print("Enter Element: "+ (i+1)+ ":");
+		for(int i=1; i<=arrayNumber; i++){
+			System.out.print("Enter Element: ");
 
 			int listElement =reader.nextInt();
 			list.add(listElement);
 		}
 
-		reader.close();
 
 		System.out.println("user List Element: "+ list);
+
+		System.out.println();
 
 		//sorting list after user input
 		Collections.sort(list);
 		System.out.println("after sorting: "+list);
+
+		//remover elements from list
+
+		System.out.print("Enter the number of elements to be removed: ");
+		int removerCount=reader.nextInt();
+
+		for(int i=1; i<=removerCount; i++) {
+			System.out.print("Enter Element Index: ");
+			int removeIndex=reader.nextInt();
+
+			if(removeIndex >=0 && removeIndex <=list.size()) {
+				list.remove(removeIndex);
+			}
+			else {
+				System.out.print("Invalid Index:" + removeIndex);
+			}
+
+		}
+
+		System.out.print("after remove Element: "+ list);
+		reader.close();
 }
 }
