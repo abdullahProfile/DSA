e8abd2b3712b490444c50c20da2e8cc65b55f7ee
Abdullah-CUI <abdullahdev88@gmail.com>
2024-09-21 11:07:12 +0500

    array list adding element method

---
+++ ArrayList/MyArrayList.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list= new ArrayList<>();
+
+        list.add(0);
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list);
+
+    }
+}
