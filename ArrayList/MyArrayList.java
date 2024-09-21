fe60ef4e9cd9f36de6ee125f70bfe1f152c10767
Abdullah-CUI <abdullahdev88@gmail.com>
2024-09-21 11:30:50 +0500

    methods:get , user input list element, sorting list

--- ArrayList/MyArrayList.java
+++ ArrayList/MyArrayList.java
@@ -1,18 +1,55 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collection;
+import java.util.Collections;
 
 public class MyArrayList {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ArrayList<Integer> list= new ArrayList<>();
+		ArrayList<Integer> list= new ArrayList<Integer>();
 
-        list.add(0);
-        list.add(1);
-        list.add(3);
-        list.add(4);
-        list.add(4);
-        list.add(5);
+		list.add(0);
+		list.add(1);
+		list.add(3);
+		list.add(4);
+		list.add(4);
+		list.add(5);
 
-        System.out.println(list);
+		System.out.println("sort list:"+ list);
 
-    }
+		//get method
+
+		int element=list.get(4);
+
+		System.out.println("Element At Index 4: "+element);
+
+		//get element by loop
+
+		System.out.println("get all list Element");
+		for(int i=0; i<list.size(); i++) {
+			System.out.print(list.get(i)+ " ");
+		}
+
+		System.out.println();
+
+		Scanner reader= new Scanner(System.in);
+
+		System.out.print("Enter Number of Element:");
+		int arrayNumber=reader.nextInt();
+
+		for(int i=0; i<=arrayNumber; i++){
+			System.out.print("Enter Element: "+ (i+1)+ ":");
+
+			int listElement =reader.nextInt();
+			list.add(listElement);
+		}
+
+		reader.close();
+
+		System.out.println("user List Element: "+ list);
+
+		//sorting list after user input
+		Collections.sort(list);
+		System.out.println("after sorting: "+list);
+}
 }
